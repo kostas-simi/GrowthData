e568e204774eee5a6e94d395a9960ffac5ff0bd1
kostas-simi <41022326+kostas-simi@users.noreply.github.com>
2019-04-20 23:43:09 +0300

    removed data outliers

--- badData.py
+++ badData.py
@@ -7,15 +7,24 @@ filepath = sys.argv[1]
 originalSheet = sys.argv[2]
 
 df = pd.read_excel(filepath, sheet_name = originalSheet)
-
 Rows = df['Tank'].count()
-
-plt.figure()
-
-ax = plt.gca()
+print(Rows)
 
 
-df = df[df['Daily Growth'] > 20 and df['Daily Growth'] < 10]
+for i in range(Rows):
+    if (df['Daily Growth'][i] > 5 or df['Daily Growth'][i] < -5):
+        df = df.drop([i])
+
+
+Rows2 = df['Tank'].count()
+print(Rows2)
+
+# --- might work ---
+
+# df2 = pd.DataFrame(index=Rows2)
+# for i in range(1,Rows2):
+#     if (df['Tank'][i] == df['Tank'][i-1] and df['Daily Growth'][i] == df['Daily Growth'][i-1]):
+#         df2[i] = df[i]
 
 
 df.to_excel('NoBad_'+filepath, sheet_name = 'output')
--- scatter.py
+++ scatter.py
@@ -6,6 +6,7 @@ import sys
 filepath = sys.argv[1]
 originalSheet = sys.argv[2]
 
+
 df = pd.read_excel(filepath, sheet_name = originalSheet)
 
 plt.figure()
@@ -14,10 +15,3 @@ ax = plt.gca()
 
 plt.scatter(x=df['Average Temp'],y=df['Daily Growth'])
 plt.show()
-
-# plt.scatter(x=df['Age_days'],y=df['Daily Growth'])
-# plt.show()
-#
-# plt.scatter(x=df['Days Apart'],y=df['Weight Difference'])
-#
-# plt.show()
